3407c9c03b54d68fadf1ac7b1f71ebba79ed68d7
Kellam <Florianguenec@hotmail.fr>
2016-11-07 02:15:51 +0100

    conversion en csv

--- src/main/webapp/app/entities/partenaire/partenaire.controller.js
+++ src/main/webapp/app/entities/partenaire/partenaire.controller.js
@@ -5,15 +5,15 @@
         .module('projectTaaGliApp')
         .controller('PartenaireController', PartenaireController);
 
-    PartenaireController.$inject = ['$scope', '$state', 'Partenaire', 'PartenaireSearch'];
+    PartenaireController.$inject = ['$scope', '$state','$log', 'Partenaire', 'PartenaireSearch'];
 
-    function PartenaireController ($scope, $state, Partenaire, PartenaireSearch) {
+    function PartenaireController ($scope, $state,$log, Partenaire, PartenaireSearch) {
         var vm = this;
-        
+
         vm.partenaires = [];
         vm.search = search;
         vm.loadAll = loadAll;
-
+        vm.exporter = exporter;
         loadAll();
 
         function loadAll() {
@@ -29,5 +29,27 @@
             PartenaireSearch.query({query: vm.searchQuery}, function(result) {
                 vm.partenaires = result;
             });
-        }    }
+        }
+        function exporter(){
+                var array = JSON.parse(vm.partenaires);
+
+                var str = '';
+
+                for (var i = 0; i < array.length; i++) {
+                    var line = '';
+
+                    for (var index in array[i]) {
+                        line += array[i][index] + ',';
+                    }
+
+                    line.slice(0,line.Length-1);
+
+                    str += line + '\r\n';
+                }
+                window.open( "data:text/csv;charset=utf-8," + escape(str))
+            }
+
+        }
+
+
 })();
