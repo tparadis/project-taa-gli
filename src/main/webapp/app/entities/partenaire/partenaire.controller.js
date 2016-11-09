53a38ca8cba8fe57d652e28904a5a8fb09fbba2f
tparadis <theophile.paradis@gmail.com>
2016-11-09 10:44:51 +0100

    exporter en csv

--- src/main/webapp/app/entities/partenaire/partenaire.controller.js
+++ src/main/webapp/app/entities/partenaire/partenaire.controller.js
@@ -13,7 +13,7 @@
         vm.partenaires = [];
         vm.search = search;
         vm.loadAll = loadAll;
-        vm.exporter = exporter;
+        $scope.exporter = exporter;
         loadAll();
 
         function loadAll() {
@@ -31,15 +31,21 @@
             });
         }
         function exporter(){
-                var array = JSON.parse(vm.partenaires);
+        console.log("export");
+        console.log(vm.partenaires);
+                var array = vm.partenaires;
 
-                var str = '';
+                var str = 'ID,Siret,Service ,Region,Code Activity,Rue ,Cplt Rue ,Code Dep ,Ville ,Tel ,Std ,Url ,Commentaire, Nom Signataire ,Effectif, Date Maj, Users, Stagiaires ,Stages \r\n';
 
-                for (var i = 0; i < array.length; i++) {
+                for (var i = 0; i < array.length ; i++) {
                     var line = '';
 
                     for (var index in array[i]) {
-                        line += array[i][index] + ',';
+                        console.log(index);
+                        if (index == "toJSON"){
+                            console.log("coucou")
+                           break;
+                        }line += array[i][index] + ',';
                     }
 
                     line.slice(0,line.Length-1);
